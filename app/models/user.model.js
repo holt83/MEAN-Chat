6e7474b87303646f56f61932fe8d5c73dd1b1e3f
Simon Holt <simonholt83@gmail.com>
2017-12-28 03:11:57 +0100

    Simplify user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -2,13 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: {
-    type: String,
-    index: true, // Secondary index.
-    match: /.+\@.+\..+/ // Pre-defined validator using a regex.
-  },
   username: {
     type: String,
     trim: true,
@@ -28,20 +21,6 @@ const UserSchema = new Schema({
   created: {
     type: Date,
     default: Date.now
-  },
-  role: {
-    type: String,
-    enum: ['Admin', 'Owner', 'User'] // Enum validator
-  },
-  website: {
-    type: String,
-    // Custom getter modifier.
-    get: function(url) {
-      if (url && url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
-        url = 'http://' + url;
-      }
-      return url;
-    }
   }
 });
 
@@ -54,18 +33,5 @@ UserSchema.statics.findOneByUsername = function (username, callback) {
 UserSchema.methods.authenticate = function (password) {
   return this.password === password;
 };
-
-// Virtual field.
-UserSchema.virtual('fullName').get(function () {
-  return this.firstName + ' ' + this.lastName;
-}).set(function (fullName) {
-  const splitName = fullName.split(' ');
-  this.firstName = splitName[0] || '';
-  this.lastName = splitName[1] || '';
-});
-
-// If we didn't do this, Mongoose would ignore our setter modifiers
-// and virtual fields.
-UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
