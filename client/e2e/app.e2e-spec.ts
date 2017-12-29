122c4e24bab55c2e58eaa17f3615b138ec53471f
Simon Holt <simonholt83@gmail.com>
2017-12-29 11:45:41 +0100

    Implement simple demonstration of Angular e2e testing

--- client/e2e/app.e2e-spec.ts
+++ client/e2e/app.e2e-spec.ts
@@ -9,6 +9,28 @@ describe('public App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('MEAN Chat');
+  });
+
+  it('It should display login screen with two input fields', () => {
+    page.navigateTo();
+    expect(page.getInputFields().count()).toEqual(2);
+  });
+
+  it('It should navigate to the Rooms listing after login in', () => {
+    page.navigateTo();
+    let usernameField = page.getUsernameField();
+    usernameField.clear();
+    let passwordField = page.getPasswordField();
+    passwordField.clear();
+
+    usernameField.sendKeys('testman');
+    passwordField.sendKeys('1234');
+
+    let loginButton = page.getLoginButton();
+    loginButton.click();
+
+    // Verify that we're on the rooms listing at there's a create button present.
+    expect(page.getCreateRoomButton().getText()).toEqual('Create');
   });
 });
--- client/e2e/app.po.ts
+++ client/e2e/app.po.ts
@@ -8,4 +8,24 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getInputFields() {
+    return element.all(by.css('app-root app-user-login input'));
+  }
+
+  getUsernameField() {
+    return element(by.css('app-user-login .username'));
+  }
+
+  getPasswordField() {
+    return element(by.css('app-user-login input[type=password]'));
+  }
+
+  getLoginButton() {
+    return element(by.css('app-user-login button'));
+  }
+
+  getCreateRoomButton() {
+    return element(by.css('app-rooms .create-room'));
+  }
 }
